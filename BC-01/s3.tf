52ff75528c54746ca7b982cffcba741110635ec7
ajay <ajaybas141@gmail.com>
2025-03-29 22:43:00 +0530

    path modified

--- BC-01/s3.tf
+++ BC-01/s3.tf
@@ -1,7 +1,7 @@
 data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "this" {
-  bucket = "bc-sthree-${data.aws_caller_identity.current.account_id}-01"
+  bucket = "bc-sthree-${data.aws_caller_identity.current.account_id}-02"
 
   tags = {
     Name        = "My bucket"
